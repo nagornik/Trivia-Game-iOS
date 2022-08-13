46e2199014826c060b6d5f5955de4ee92c8a90bf
Anton Nagorny <nagorny.anton@gmail.com>
2022-08-13 23:33:27 +0300

    – Refactor button

--- Trivia/Components/DiffButton.swift
+++ Trivia/Components/DiffButton.swift
@@ -8,33 +8,30 @@
 import SwiftUI
 
 struct DiffButton: View {
-
+    
     @EnvironmentObject var triviaManager: TriviaManager
     
     var isItDifficulty: Bool
     var text: String
-
+    
     var body: some View {
-            
+        
         VStack {
             if isItDifficulty {
                 Text(text)
-                    .foregroundColor(.white)
-                    .bold()
                     .padding(.horizontal)
                     .padding(.vertical, 8)
                     .background(triviaManager.selectedDifficulty.rawValue == text ? Color("accent") : .gray.opacity(0.5))
-                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
             } else {
                 Text(text.deletingPrefix("Entertainment: "))
-                    .foregroundColor(.white)
-                    .bold()
                     .padding(.horizontal)
                     .padding(.vertical, 8)
                     .background(triviaManager.selectedCategory.name == text ? Color("accent") : .gray.opacity(0.5))
-                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
             }
         }
+        .foregroundColor(.white)
+        .font(.body.bold())
+        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
         
     }
 }
